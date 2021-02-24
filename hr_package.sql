4fb5194834902e2778e2fb91b7378a36cd201add
bahar-j <joonie811@naver.com>
2021-02-24 14:36:11 +0900

    feat: add updated count by rowcount and commit/rollback

--- hr_package.sql
+++ hr_package.sql
@@ -19,20 +19,32 @@ IS
 
       INSERT INTO employees2 (employee_id, last_name, email, job_id, hire_date)
         VALUES(employees_seq.nextval, ps_emp_name, pe_email, pj_job_id, TO_DATE(pd_hire_date, 'yyyy-mm-dd'));
+      COMMIT;
 
     EXCEPTION
       WHEN e_no_such_job THEN
         dbms_output.put_line('해당하는 JOB_ID가 없습니다.');
-    END;
+        ROLLBACK;
+      WHEN OTHERS THEN
+        ROLLBACK;
+    END add_emp_proc;
 
     PROCEDURE retire_emp_proc(pn_employee_id IN NUMBER)
     IS
     BEGIN
-        UPDATE employees2 SET retire_date = sysdate
-        WHERE employee_id = pn_employee_id;
+        UPDATE employees2 
+          SET retire_date = SYSDATE
+          WHERE employee_id = pn_employee_id
+          AND retire_date IS NULL;
+
+        dbms_output.put_line(SQL%ROWCOUNT || '건 업데이트 되었습니다.');
+        COMMIT;
 
     EXCEPTION
       WHEN NO_DATA_FOUND THEN
         dbms_output.put_line('해당하는 사원이 없습니다.');
-    END;
+        ROLLBACK;
+      WHEN OTHERS THEN
+        ROLLBACK;
+    END retire_emp_proc;
 END hr_pkg;
